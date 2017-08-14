2ec6238507b84eb731d33256ac0a33e366f4e487
josepotal <j.otal.pares@gmail.com>
2017-08-14 17:01:50 -0500

    Complete authentication flow, adding cookie-sessionand logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,4 +9,19 @@ module.exports = app => {
   );
 
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get('/api/current_user', (req, res) => {
+    /* cookie data is stored in req.session thanks to cookie-session package
+    passport accesses data into req.session
+
+    res.send(req.session);
+    */
+
+    res.send(req.user);
+  });
 };
